69adddd5631274fce263483694eebb4da69e0de2
WHtra <WHtra@DESKTOP-BS5PLKM>
2021-06-12 12:57:04 +0900

    Exam 2

--- src/java_exam/exam1.java
+++ src/java_exam/exam1.java
@@ -2,7 +2,8 @@ package java_exam;
 
 public class exam1 {
 	public static void main(String[] args) {
-		System.out.println("gitHub Exam!");
+		System.out.println("gitHub Exam1");
+		System.out.println("gitHub Exam2");
 	}
 
 }
